11edea1d684c442faacfb31d10a838bb4b029841
Joseph Allen <joseph.allen@mines.sdsmt.edu>
2020-02-22 13:07:56 -0700

    speed controller library modified to use interrupts.

---
+++ SpeedController Library/SpeedControllerNew.cpp
@@ -0,0 +1,122 @@
+/* 1/18/2020
+* Christian Weaver & Samuel Ryckman
+*
+* SpeedController class.
+* This file provides function definitions to regulate the power outputted to a motor to maintain a given speed.
+*/
+ 
+#include "SpeedControllerNew.h"
+ 
+SpeedController::SpeedController( AS5134 *encoder) {}
+ 
+int SpeedController::motorSpeedToPower(int desiredMotorSpeed)
+{
+   //Compute the motor power by comparing the filtered motor
+   //speed and the desired motor speed
+   return computeMotorPower(filterMotorSpeed(getSpeed()), desiredMotorSpeed);
+}
+ 
+int SpeedController::computeMotorPower(int motorSpeed, int desiredSpeed)
+{
+   //calculate and store time info
+   static long lastTime = 0;
+   static int lastError = 0;
+   long curTime = micros();
+   float elapsedTime;
+ 
+   //Define general variables
+   static float integral;
+   float derivative = 0;
+   int output = 0;
+   int error = 0;
+ 
+   // Skip if first loop to set up initial values
+   if (lastTime == 0)
+   {
+       lastTime = micros();
+       integral = 0;
+       lastError = desiredSpeed - motorSpeed;
+       for (int i = 0; i < 5; i++)
+       {
+           filterMotorSpeed(motorSpeed);
+       }
+       return (maxOutput + minOutput) / 2;
+   }
+ 
+   //update the time
+   elapsedTime = (curTime - lastTime) / 1000000.0;
+   lastTime = curTime;
+ 
+   //get the error
+   error = desiredSpeed - motorSpeed;
+ 
+   //Get the integral of the error
+   integral += float(error) * elapsedTime;
+ 
+   //constrain acumulator
+   constrain(integral,minAcumulator,maxAcumulator);
+ 
+   //Get the derivative of the error
+   derivative = float((error) - lastError) / elapsedTime;
+ 
+   //calculate the output
+   output = kp * error + ki * integral + kd * derivative;
+ 
+   //cap the output and return
+   return constrain(output, minOutCap, maxOutCap);
+}
+ 
+int SpeedController::filterMotorSpeed(int curMotorSpeed)
+{
+   static int pastVals[] = {0, 0, 0, 0, 0, 0, 0};
+   const int numVals = 7;
+   static int curIndex = 0;
+   static long total = 0;
+ 
+   //subtract the outdated value
+   total -= pastVals[curIndex];
+ 
+   //add the new value to the array
+   pastVals[curIndex] = curMotorSpeed/numVals;
+ 
+   //add the new value to the total
+   total +=pastVals[curIndex];
+ 
+   //update position in the rolling array
+   curIndex++;
+   if (curIndex >= numVals)
+   {
+       curIndex = 0;
+   }
+ 
+   return total;
+}
+ 
+int SpeedController::getSpeed()
+{
+   static unsigned long curTime, lastTime  = micros();
+   static int curPos, lastPos  = encoder->readMultiTurnAngle();
+ 
+   //Calculate the speed based on the current position
+   curPos = 60*count;
+   curTime = micros();
+   curMotorSpeed1 = long(curPos - lastPos) * 166667 / (curTime - lastTime); //rpm
+ 
+   //reset counter and reset set last pos and time to new values
+   count = 0;
+   lastPos = encoder->readMultiTurnAngle();
+   lastTime = micros();
+ 
+   return curMotorSpeed1;
+}
+ 
+int SpeedController::getSpeed1()
+{
+   return curMotorSpeed1;
+}
+
+ void SpeedController::upCount()
+ {
+     ++count;
+     return;
+ }
---
+++ SpeedController Library/SpeedControllerNew.h
@@ -0,0 +1,53 @@
+/* 1/18/2020
+ * Christian Weaver & Samuel Ryckman
+ *
+ * Header for the SpeedController class
+ */
+
+#ifndef SPEEDCONTROLLER
+#define SPEEDCONTROLLER
+
+//#include <cmath>
+#include "AS5134.h"
+#include "Arduino.h"
+
+using namespace std;                                                           
+
+class SpeedController
+{
+public:
+    SpeedController();
+    int getSpeed1();
+        
+    int motorSpeedToPower(int desiredSpeed);
+
+
+private:
+    AS5134* encoder;
+    const int maxOutput = 255;    //max output value
+    const int minOutput = -255;   //min output value
+    const int maxOutCap = 40;     //max output we will send
+    const int minOutCap = -40;    //min output we will send
+    const int maxInput = 18000;  //max possible input value
+    const int minInput = -18000; //min possible input value
+    const float maxAcumulator = 70;
+    const float minAcumulator = -70;
+
+    int curMotorSpeed1;
+    int count =0;
+    
+    //Define PID constants
+    const float kp = float(abs(maxOutput - minOutput)) / abs(maxInput - minInput);
+    const float ki = 1;
+    const float kd = 0.004;
+
+    int computeMotorPower(int motorSpeed, int desiredMotorSpeed);
+    int filterMotorSpeed(int curMotorSpeed);
+    int getSpeed();
+    void upCount();
+};
+
+
+
+
+#endif
